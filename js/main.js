df6da19228af0c79e6b7af61c3d2479a0cae0584
Thereza Bandoli <tcbandolilegg@gmail.com>
2020-09-20 10:23:34 -0300

    Exemplos de alert,  up e lowerCase, console.log

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+var nome = "Thereza Cristina";
+var idade = 54;
+alert(nome + " tem " + idade + " anos");
+var frase = "Trabalhando  com Conceitos do Java";
+console.log("Vendo atraves do console.log");
+console.log("Original => " + frase);
+console.log("tudo em maiusculas => " + frase.toUpperCase());
+console.log("tudo em minusculas => " + frase.toLowerCase());
 
-alert("Meu primeiro script");
+var frase2 = "thereza é mãe";
+console.log("Original => " + frase2);
+console.log("Utilzando o .replace " + frase2.replace("mãe", "pai"));
+
+
+
